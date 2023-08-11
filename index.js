7cd752c47a995135b733edaa7bb8fafd6a49fda7
giraud-alexis <140321581+giraud-alexis@users.noreply.github.com>
2023-08-11 18:36:06 +0200

    maj

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+let nav = document.querySelector("#side-bar");
+const btn = document.querySelector("#btn");
+console.log(btn);
+
+const sidebar = document.querySelector("#side-bar");
+
+btn.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+});
+
 // Créer un événement au click sur le hamburger menu pour sortir la sidebar, ranger la sidebar si on reclick dessus
 
 // Ranger la sidebar si on click sur le contenu principal
